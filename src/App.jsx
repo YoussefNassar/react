c425891fbf5ff36ee35689c2d96f033d1c96d6fc
Youssef Nassar <youssef.nassar@solactive.com>
2023-06-16 17:49:17 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { createRoot } from "react-dom";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams/>
+      {/* <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" /> */}
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
