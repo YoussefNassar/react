8b05aab6679fc21f52944b27135b3a9630519362
Youssef Nassar <youssef.nassar@solactive.com>
2023-06-26 17:34:55 +0200

    use react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import { createRoot } from "react-dom";
-// import Pet from "./Pet";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams/>
-      {/* <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" /> */}
-    </div>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <h1>Adopt Me!</h1>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 };
 
