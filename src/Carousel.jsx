f99c94d2cbd5e9e65ada2e1de1ba6468fd2bdc49
Youssef Nassar <youssef.nassar@solactive.com>
2023-06-27 13:23:20 +0200

    change photo when on details page

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -9,25 +9,34 @@ class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
+  handleIndexClick = (e) => {
+    this.setState({
+      active: +e.target.dataset.index,
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
 
     return (
       <div className="carousel">
-        <img src={images[active]} alt="animal hero"/>
+        <img src={images[active]} alt="animal" />
         <div className="carousel-smaller">
-            {images.map((photo, index) => (
-                <img 
-                    key={photo}
-                    src={photo}
-                    className={index === active ? "active":""}
-                    alt="animal thumbnail"
-                />
-            ))}
+          {images.map((photo, index) => (
+            // eslint-disable-next-linex
+            <img
+              onClick={this.handleIndexClick}
+              data-index={index}
+              key={photo}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
         </div>
       </div>
     );
   }
 }
- export default Carousel;
+export default Carousel;
